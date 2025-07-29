0e74df64aaa8a8b183718201ffddb6d3010094eb
Harsh Kumar <kunarharsh76@gmail.com>
2025-07-29 15:24:22 +0530

    Add files via upload

---
+++ EXP7-g.cpp
@@ -0,0 +1,20 @@
+//Harsh kumar
+//24070123146
+//B3
+#include <iostream>
+using namespace std;
+int main(){
+    string a;
+    cout<< "enter the string: ";
+    cin>>a;
+    int v =a.length();
+    cout << "reversed string is: ";
+    for (int i=0;i<=v;i++){
+       cout << a[v-i];
+    }
+}
+
+/*Output : 
+enter the string: harsh 
+hsrah
+*/
